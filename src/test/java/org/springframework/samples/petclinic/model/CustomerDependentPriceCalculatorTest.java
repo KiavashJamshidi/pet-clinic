2d7802485b3769ec36cfbf33bb77132cbc537cd2
Emma Paris <eparis@atlassian.com>
2021-11-15 19:40:21 +0330

    ca3 done

--- src/test/java/org/springframework/samples/petclinic/model/CustomerDependentPriceCalculatorTest.java
+++ src/test/java/org/springframework/samples/petclinic/model/CustomerDependentPriceCalculatorTest.java
@@ -4,6 +4,8 @@ import org.junit.BeforeClass;
 import org.junit.AfterClass;
 import org.junit.Test;
 import org.springframework.samples.petclinic.model.priceCalculators.CustomerDependentPriceCalculator;
+
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 import java.util.Date;
 import java.util.*;
@@ -11,41 +13,47 @@ import java.util.*;
 import static org.junit.Assert.assertEquals;
 
 public class CustomerDependentPriceCalculatorTest{
-    private CustomerDependentPriceCalculator customerDependentPriceCalculator;
-    private double BASE_RARE_COEF;
-    private double baseCharge;
-	private double BASE_PRICE_PER_PET;
-    private double RARE_INFANCY_COEF;
-	private double COMMON_INFANCY_COEF;
-	private UserType newUser;
-	private UserType goldUser;
-	private UserType silverUser;
+	private static CustomerDependentPriceCalculator customerDependentPriceCalculator;
+	private static double BASE_RARE_COEF;
+	private static double baseCharge;
+	private static double BASE_PRICE_PER_PET;
+	private static double RARE_INFANCY_COEF;
+	private static double COMMON_INFANCY_COEF;
+	private static UserType newUser;
+	private static UserType goldUser;
+	private static UserType silverUser;
+	private static Pet pet;
+	private static PetType petType;
 
-    @BeforeClass
-    public void setUp(){
-        this.customerDependentPriceCalculator = new CustomerDependentPriceCalculator();
-        this.BASE_RARE_COEF = 1.2;
-        this.baseCharge = 100;
-        this.BASE_PRICE_PER_PET = 40;
-        this.RARE_INFANCY_COEF = 1.4;
-        this.COMMON_INFANCY_COEF = 1.2;
-        this.newUser = UserType.NEW;
-        this.goldUser = UserType.GOLD;
-        this.silverUser = UserType.SILVER;
-    }
+	@BeforeClass
+	public static void setUp(){
+		customerDependentPriceCalculator = new CustomerDependentPriceCalculator();
+		BASE_RARE_COEF = 1.2;
+		baseCharge = 100;
+		BASE_PRICE_PER_PET = 40;
+		RARE_INFANCY_COEF = 1.4;
+		COMMON_INFANCY_COEF = 1.2;
+		newUser = UserType.NEW;
+		goldUser = UserType.GOLD;
+		silverUser = UserType.SILVER;
+		pet = mock(Pet.class) ;
+		petType= mock(PetType.class);
+		when(pet.getType()).thenReturn(petType);
 
-    @Test
+	}
+
+	@Test
 	public void should_returnZero_when_petsIsEmptyAndUserIsSilver() {
-        List<Pet> emtpyList = Collections.emptyList();
+		List<Pet> emtpyList = Collections.emptyList();
 
 		double calculatedPrice = customerDependentPriceCalculator.calcPrice(emtpyList, baseCharge, BASE_PRICE_PER_PET, silverUser);
 
 		assertEquals(0, calculatedPrice, 0.01);
 	}
 
-    @Test
+	@Test
 	public void should_returnBaseCharge_when_petsIsEmptyAndUserIsGold() {
-        List<Pet> emtpyList = Collections.emptyList();
+		List<Pet> emtpyList = Collections.emptyList();
 
 		double calculatedPrice = customerDependentPriceCalculator.calcPrice(emtpyList, baseCharge, BASE_PRICE_PER_PET, goldUser);
 
@@ -53,32 +61,28 @@ public class CustomerDependentPriceCalculatorTest{
 	}
 
 
-    @Test
+	@Test
 	public void should_returnSpecifidPrice_when_lengthOfPetsIsOneAndPetIsNotRareAndPetIsNotInfantAndUserIsGold() {
-        List<Pet> pets = Collections.emptyList();
-        Pet pet = new Pet();
-        pets.add(pet);
-        Calendar calendar = Calendar.getInstance();
+		List<Pet> pets = Arrays.asList(pet);
+		Calendar calendar = Calendar.getInstance();
 		calendar.set(Calendar.YEAR, 2010);
 		Date adultAge = calendar.getTime();
 
-        when(pet.getType().getRare()).thenReturn(false);
-        when(pet.getBirthDate()).thenReturn(adultAge);
+		when(pet.getType().getRare()).thenReturn(false);
+		when(pet.getBirthDate()).thenReturn(adultAge);
 
 		double calculatedPrice = customerDependentPriceCalculator.calcPrice(pets, baseCharge, BASE_PRICE_PER_PET, goldUser);
 
 		assertEquals(baseCharge + (BASE_PRICE_PER_PET * goldUser.discountRate), calculatedPrice, 0.01);
 	}
 
-    @Test
+	@Test
 	public void should_returnSpecifidPrice_when_lengthOfPetsIsOneAndPetIsNotRareAndPetIsInfantAndUserIsGold() {
-        List<Pet> pets = Collections.emptyList();
-        Pet pet = new Pet();
-        pets.add(pet);
+		List<Pet> pets = Arrays.asList(pet);
 		Date infantAge = new Date();
 
-        when(pet.getType().getRare()).thenReturn(false);
-        when(pet.getBirthDate()).thenReturn(infantAge);
+		when(pet.getType().getRare()).thenReturn(false);
+		when(pet.getBirthDate()).thenReturn(infantAge);
 
 		double calculatedPrice = customerDependentPriceCalculator.calcPrice(pets, baseCharge, BASE_PRICE_PER_PET, goldUser);
 
@@ -86,161 +90,122 @@ public class CustomerDependentPriceCalculatorTest{
 	}
 
 
-    @Test
+	@Test
 	public void should_returnSpecifidPrice_when_lengthOfPetsIsOneAndPetIsNotRareAndPetIsInfantAndUserIsSilver() {
-        List<Pet> pets = Collections.emptyList();
-        Pet pet = new Pet();
-        pets.add(pet);
+		List<Pet> pets = Arrays.asList(pet);
 		Date infantAge = new Date();
 
-        when(pet.getType().getRare()).thenReturn(false);
-        when(pet.getBirthDate()).thenReturn(infantAge);
+		when(pet.getType().getRare()).thenReturn(false);
+		when(pet.getBirthDate()).thenReturn(infantAge);
 
 		double calculatedPrice = customerDependentPriceCalculator.calcPrice(pets, baseCharge, BASE_PRICE_PER_PET, silverUser);
 
 		assertEquals(BASE_PRICE_PER_PET * COMMON_INFANCY_COEF, calculatedPrice, 0.01);
 	}
 
-    @Test
+	@Test
 	public void should_returnSpecifidPrice_when_lengthOfPetsIsOneAndPetIsNotRareAndPetIsNotInfantAndUserIsSilver() {
-        List<Pet> pets = Collections.emptyList();
-        Pet pet = new Pet();
-        pets.add(pet);
-        Calendar calendar = Calendar.getInstance();
+		List<Pet> pets = Arrays.asList(pet);
+		Calendar calendar = Calendar.getInstance();
 		calendar.set(Calendar.YEAR, 2010);
 		Date adultAge = calendar.getTime();
 
-        when(pet.getType().getRare()).thenReturn(false);
-        when(pet.getBirthDate()).thenReturn(adultAge);
+		when(pet.getType().getRare()).thenReturn(false);
+		when(pet.getBirthDate()).thenReturn(adultAge);
 
 		double calculatedPrice = customerDependentPriceCalculator.calcPrice(pets, baseCharge, BASE_PRICE_PER_PET, silverUser);
 
 		assertEquals(BASE_PRICE_PER_PET, calculatedPrice, 0.01);
 	}
 
-    @Test
+	@Test
 	public void should_returnSpecifidPrice_when_lengthOfPetsIsOneAndPetIsRareAndPetIsNotInfantAndUserIsSilver() {
-        List<Pet> pets = Collections.emptyList();
-        Pet pet = new Pet();
-        pets.add(pet);
-        Calendar calendar = Calendar.getInstance();
+		List<Pet> pets = Arrays.asList(pet);
+		Calendar calendar = Calendar.getInstance();
 		calendar.set(Calendar.YEAR, 2010);
 		Date adultAge = calendar.getTime();
 
-        when(pet.getType().getRare()).thenReturn(true);
-        when(pet.getBirthDate()).thenReturn(adultAge);
+		when(pet.getType().getRare()).thenReturn(true);
+		when(pet.getBirthDate()).thenReturn(adultAge);
 
 		double calculatedPrice = customerDependentPriceCalculator.calcPrice(pets, baseCharge, BASE_PRICE_PER_PET, silverUser);
 
 		assertEquals(BASE_PRICE_PER_PET * BASE_RARE_COEF, calculatedPrice, 0.01);
 	}
 
+	@Test
 	public void should_returnSpecifidPrice_when_lengthOfPetsIsOneAndPetIsRareAndPetIsNotInfantAndUserIsGold() {
-        List<Pet> pets = Collections.emptyList();
-        Pet pet = new Pet();
-        pets.add(pet);
-        Calendar calendar = Calendar.getInstance();
+		List<Pet> pets = Arrays.asList(pet);
+		Calendar calendar = Calendar.getInstance();
 		calendar.set(Calendar.YEAR, 2010);
 		Date adultAge = calendar.getTime();
 
-        when(pet.getType().getRare()).thenReturn(true);
-        when(pet.getBirthDate()).thenReturn(adultAge);
+		when(pet.getType().getRare()).thenReturn(true);
+		when(pet.getBirthDate()).thenReturn(adultAge);
 
 		double calculatedPrice = customerDependentPriceCalculator.calcPrice(pets, baseCharge, BASE_PRICE_PER_PET, goldUser);
 
 		assertEquals((BASE_PRICE_PER_PET * BASE_RARE_COEF * goldUser.discountRate) + baseCharge, calculatedPrice, 0.01);
 	}
 
-    @Test
+	@Test
 	public void should_returnSpecifidPrice_when_lengthOfPetsIsOneAndPetIsRareAndPetIsInfantAndUserIsSilver() {
-        List<Pet> pets = Collections.emptyList();
-        Pet pet = new Pet();
-        pets.add(pet);
+		List<Pet> pets = Arrays.asList(pet);
 		Date infantAge = new Date();
 
-        when(pet.getType().getRare()).thenReturn(true);
-        when(pet.getBirthDate()).thenReturn(infantAge);
+		when(pet.getType().getRare()).thenReturn(true);
+		when(pet.getBirthDate()).thenReturn(infantAge);
 
 		double calculatedPrice = customerDependentPriceCalculator.calcPrice(pets, baseCharge, BASE_PRICE_PER_PET, silverUser);
 
 		assertEquals(BASE_PRICE_PER_PET * BASE_RARE_COEF * RARE_INFANCY_COEF, calculatedPrice, 0.01);
 	}
 
-    @Test
+	@Test
 	public void should_returnSpecifidPrice_when_lengthOfPetsIsOneAndPetIsRareAndPetIsInfantAndUserIsGold() {
-        List<Pet> pets = Collections.emptyList();
-        Pet pet = new Pet();
-        pets.add(pet);
+		List<Pet> pets = Arrays.asList(pet);
 		Date infantAge = new Date();
 
-        when(pet.getType().getRare()).thenReturn(true);
-        when(pet.getBirthDate()).thenReturn(infantAge);
+		when(pet.getType().getRare()).thenReturn(true);
+		when(pet.getBirthDate()).thenReturn(infantAge);
 
 		double calculatedPrice = customerDependentPriceCalculator.calcPrice(pets, baseCharge, BASE_PRICE_PER_PET, goldUser);
 
 		assertEquals((BASE_PRICE_PER_PET * BASE_RARE_COEF * RARE_INFANCY_COEF * goldUser.discountRate) + baseCharge,
-            calculatedPrice, 0.01);
+			calculatedPrice, 0.01);
 	}
 
-    @Test
-	public void should_returnSpecifidPrice_when_lengthOfPetsIsTenAndPetsAreNotRareAndPetIsNotInfantAndUserIsGold() {
-        List<Pet> pets = Collections.emptyList();
-        Pet pet = new Pet();
-        Calendar calendar = Calendar.getInstance();
+	@Test
+	public void should_returnSpecifiedPrice_when_lengthOfPetsIsTenAndPetsAreNotRareAndPetIsNotInfantAndUserIsNew() {
+		Calendar calendar = Calendar.getInstance();
 		calendar.set(Calendar.YEAR, 2010);
-        Date adultAge = calendar.getTime();
-        pet.setBirthDate(adultAge);
-
-        when(pet.getType().getRare()).thenReturn(true);
-
-        for (int i=0; i<10; i++) {
-            pets.add(pet);
-        }
-
-		double calculatedPrice = customerDependentPriceCalculator.calcPrice(pets, baseCharge, BASE_PRICE_PER_PET, goldUser);
-
-		assertEquals((BASE_PRICE_PER_PET * pets.size() * goldUser.discountRate) + baseCharge, calculatedPrice, 0.01);
-	}
-
-    @Test
-	public void should_returnSpecifidPrice_when_lengthOfPetsIsTenAndPetsAreNotRareAndPetIsNotInfantAndUserIsNew() {
-        List<Pet> pets = Collections.emptyList();
-        Pet pet = new Pet();
-        Calendar calendar = Calendar.getInstance();
-		calendar.set(Calendar.YEAR, 2010);
-        Date adultAge = calendar.getTime();
-        pet.setBirthDate(adultAge);
-
-        when(pet.getType().getRare()).thenReturn(true);
-
-        for (int i=0; i<10; i++)
-            pets.add(pet);
+		Date adultAge = calendar.getTime();
+		when(pet.getType().getRare()).thenReturn(false);
+		when(pet.getBirthDate()).thenReturn(adultAge);
+		List<Pet> pets = Arrays.asList(pet, pet, pet, pet, pet, pet, pet, pet, pet, pet);
 
 		double calculatedPrice = customerDependentPriceCalculator.calcPrice(pets, baseCharge, BASE_PRICE_PER_PET, newUser);
 
 		assertEquals((BASE_PRICE_PER_PET * pets.size() * newUser.discountRate) + baseCharge, calculatedPrice, 0.01);
 	}
-    
-	public void should_returnSpecifidPrice_when_lengthOfPetsIsTenAndPetsAreNotRareAndPetIsNotInfantAndUserIsSilver() {
-        List<Pet> pets = Collections.emptyList();
-        Pet pet = new Pet();
-        Calendar calendar = Calendar.getInstance();
+
+	@Test
+	public void should_returnSpecifiedPrice_when_lengthOfPetsIsTenAndPetsAreNotRareAndPetIsNotInfantAndUserIsSilver() {
+		Calendar calendar = Calendar.getInstance();
 		calendar.set(Calendar.YEAR, 2010);
-        Date adultAge = calendar.getTime();
-        pet.setBirthDate(adultAge);
+		Date adultAge = calendar.getTime();
+		when(pet.getBirthDate()).thenReturn(adultAge);
+		when(pet.getType().getRare()).thenReturn(false);
 
-        when(pet.getType().getRare()).thenReturn(true);
-
-        for (int i=0; i<10; i++)
-            pets.add(pet);
+		List<Pet> pets = Arrays.asList(pet, pet, pet, pet, pet, pet, pet, pet, pet, pet);
 
 		double calculatedPrice = customerDependentPriceCalculator.calcPrice(pets, baseCharge, BASE_PRICE_PER_PET, silverUser);
 
 		assertEquals(((BASE_PRICE_PER_PET * pets.size()) + baseCharge) * silverUser.discountRate , calculatedPrice, 0.01);
 	}
 
-    @AfterClass
-    public void tearDown(){
-        this.customerDependentPriceCalculator = null;
-    }
+	@AfterClass
+	public static void tearDown(){
+		customerDependentPriceCalculator = null;
+	}
 }
--- src/test/java/org/springframework/samples/petclinic/model/SimplePriceCalculatorTest.java
+++ src/test/java/org/springframework/samples/petclinic/model/SimplePriceCalculatorTest.java
@@ -4,76 +4,78 @@ import org.junit.AfterClass;
 import org.springframework.samples.petclinic.model.priceCalculators.SimplePriceCalculator;
 import org.junit.Test;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 public class SimplePriceCalculatorTest {
-	private double BASE_RARE_COEF;
-    private double baseCharge;
-	private double BASE_PRICE_PER_PET;
-	private UserType newUser;
-	private UserType oldUser ;
-    private SimplePriceCalculator simplePriceCalculator;
+	private static double BASE_RARE_COEF;
+	private static double baseCharge;
+	private static double BASE_PRICE_PER_PET;
+	private static UserType newUser;
+	private static UserType oldUser ;
+	private static SimplePriceCalculator simplePriceCalculator;
+	private static Pet pet;
+	private static PetType petType;
 
-    @BeforeClass
-    public void setUp(){
-        this.BASE_RARE_COEF = 1.2;
-        this.baseCharge = 100;
-        this.BASE_PRICE_PER_PET = 40;
-        this.newUser = UserType.NEW;
-        this.oldUser = UserType.SILVER;
-        this.simplePriceCalculator = new SimplePriceCalculator();
-    }
+	@BeforeClass
+	public static void setUp(){
+		BASE_RARE_COEF = 1.2;
+		baseCharge = 100;
+		BASE_PRICE_PER_PET = 40;
+		newUser = UserType.NEW;
+		oldUser = UserType.SILVER;
+		simplePriceCalculator = new SimplePriceCalculator();
+		pet = mock(Pet.class) ;
+		petType= mock(PetType.class);
+		when(pet.getType()).thenReturn(petType);
 
-    @Test
-    //Test Path 1 2 3 8 10
-    public void should_returnBaseCharge_when_petsIsEmptyAndUserIsOld(){
-        List<Pet> emtpyList = Collections.emptyList();
-        double calculatedPrice = simplePriceCalculator.calcPrice(emtpyList, baseCharge, BASE_PRICE_PER_PET, oldUser);
-        assertEquals(this.baseCharge, calculatedPrice, 0.1);
-    }
+	}
 
-    @Test
-    //Test Path 1 2 3 8 9 10
-    public void should_returnBaseChargeMultDiscountRate_when_petsIsEmptyAndUserIsNew(){
-        List<Pet> emtpyList = Collections.emptyList();
-        double calculatedPrice = simplePriceCalculator.calcPrice(emtpyList, baseCharge, BASE_PRICE_PER_PET, newUser);
-        assertEquals(baseCharge * newUser.discountRate, calculatedPrice, 0.1);
-    }
+	@Test
+	//Test Path 1 2 3 8 10
+	public void should_returnBaseCharge_when_petsIsEmptyAndUserIsOld(){
+		List<Pet> emtpyList = Collections.emptyList();
+		double calculatedPrice = simplePriceCalculator.calcPrice(emtpyList, baseCharge, BASE_PRICE_PER_PET, oldUser);
+		assertEquals(this.baseCharge, calculatedPrice, 0.1);
+	}
+
+	@Test
+	//Test Path 1 2 3 8 9 10
+	public void should_returnBaseChargeMultDiscountRate_when_petsIsEmptyAndUserIsNew(){
+		List<Pet> emtpyList = Collections.emptyList();
+		double calculatedPrice = simplePriceCalculator.calcPrice(emtpyList, baseCharge, BASE_PRICE_PER_PET, newUser);
+		assertEquals(baseCharge * newUser.discountRate, calculatedPrice, 0.1);
+	}
 
 
-    @Test
-    //Test Path 1 2 3 4 5 7 3 8 10
-    public void should_returnExpectedPrice_when_LengthOfPetsIsOneAndUserIsOld(){
-        List<Pet> pets = Collections.emptyList();
-        Pet pet = new Pet();
-        pets.add(pet);
+	@Test
+	//Test Path 1 2 3 4 5 7 3 8 10
+	public void should_returnExpectedPrice_when_LengthOfPetsIsOneAndUserIsOld(){
+		List<Pet> pets = Arrays.asList(pet);
+		when(pet.getType().getRare()).thenReturn(true);
 
-        when(pet.getType().getRare()).thenReturn(true);
+		double calculatedPrice = simplePriceCalculator.calcPrice(pets, baseCharge, BASE_PRICE_PER_PET, oldUser);
 
-        double calculatedPrice = simplePriceCalculator.calcPrice(pets, baseCharge, BASE_PRICE_PER_PET, oldUser);
-        
-        assertEquals(baseCharge + BASE_PRICE_PER_PET * BASE_RARE_COEF, calculatedPrice, 0.1);
-    }
+		assertEquals(baseCharge + BASE_PRICE_PER_PET * BASE_RARE_COEF, calculatedPrice, 0.1);
+	}
 
-    @Test
-    //Test Path 1 2 3 4 6 7 3 8 10
-    public void should_returnExpectedPrice_when_LengthOfPetsIsOneAndUserIsNew(){
-        List<Pet> pets = Collections.emptyList();
-        Pet pet = new Pet();
-        pets.add(pet);
+	@Test
+	//Test Path 1 2 3 4 6 7 3 8 10
+	public void should_returnExpectedPrice_when_LengthOfPetsIsOneAndUserIsNew(){
+		List<Pet> pets = Arrays.asList(pet);
+		when(pet.getType().getRare()).thenReturn(false);
 
-        when(pet.getType().getRare()).thenReturn(false);
+		double calculatedPrice = simplePriceCalculator.calcPrice(pets, baseCharge, BASE_PRICE_PER_PET, newUser);
 
-        double calculatedPrice = simplePriceCalculator.calcPrice(pets, baseCharge, BASE_PRICE_PER_PET, newUser);
-        
-        assertEquals(baseCharge + BASE_PRICE_PER_PET, calculatedPrice, 0.1);
-    }
+		assertEquals((baseCharge + BASE_PRICE_PER_PET) * newUser.discountRate, calculatedPrice, 0.01);
+	}
 
-    @AfterClass
-    public void tearDown(){
-        this.simplePriceCalculator = null;
-    }
+	@AfterClass
+	public static void tearDown(){
+		simplePriceCalculator = null;
+	}
 }
